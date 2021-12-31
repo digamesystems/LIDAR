20d3a995711131aaa06ecf36b9dcc33dd61c06f8
John Price <john.price@digamesystems.com>
2021-12-31 12:01:47 -0800

    updated version number to 0.9.69

--- src/digame_include/Digame/digameVersion.h
+++ src/digame_include/Digame/digameVersion.h
@@ -1,7 +1,7 @@
 #ifndef __DIGAME_VERSION_H__
 #define __DIGAME_VERSION_H__
 
-const String SW_VERSION       = "0.9.68";
-const String TERSE_SW_VERSION = "0968";  
+const String SW_VERSION       = "0.9.69";
+const String TERSE_SW_VERSION = "0969";  
 
 #endif
